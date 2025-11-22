1e87fcbaddd55cbc2bacaf5c5952bcec50c71a0d
Aricmo <momoorejohnson@gmail.com>
2025-11-22 23:29:04 +0100

    completed lumusi_intro
    Added dialogue, and character animation

---
+++ scenes/quests/story_quests/lumusi/lumusi_intro/lumusi_cinematic.gd
@@ -0,0 +1,35 @@
+extends Cinematic
+
+
+## Dialogue for cinematic scene.
+@export var lumusi_dialogue: DialogueResource = preload("res://lumusi-intro.dialogue")
+
+## Optional animation player, to be used from [member dialogue] (if needed).
+@export var animation_player2: AnimationPlayer
+
+## Optional scene to switch to once [member dialogue] is complete.
+#@export_file("*.tscn") var next_scene: String
+
+## Optional path inside [member next_scene] where the player should appear.
+## If blank, player appears at default position in the scene. If in doubt,
+## leave this blank.
+#@export var spawn_point_path: String
+
+
+func _ready() -> void:
+	if not GameState.intro_dialogue_shown:
+		DialogueManager.show_dialogue_balloon(dialogue, "", [self])
+		await DialogueManager.dialogue_ended
+		cinematic_finished.emit()
+		GameState.intro_dialogue_shown = true
+
+	if next_scene:
+		(
+			SceneSwitcher
+			. change_to_file_with_transition(
+				next_scene,
+				spawn_point_path,
+				Transition.Effect.FADE,
+				Transition.Effect.FADE,
+			)
+		)
